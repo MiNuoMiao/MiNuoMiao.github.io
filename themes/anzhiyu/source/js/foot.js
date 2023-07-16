d6bb46751d3c13b83124b0d7e7abb9662f215645
xiaomao <3172995085@qq.com>
2023-07-16 18:40:57 +0800

    页面美化

--- themes/anzhiyu/source/js/foot.js
+++
@@ -1,4 +0,0 @@
-// 页脚 by文字
-$(document).ready(function (e) {
-  $('.copyright').html('<p style="color: #A7B5E9;">©2023 <i class="anzhiyufont anzhiyu-icon-heartbeat" style="color: red"></i> By 诺喵</p>')
-})
