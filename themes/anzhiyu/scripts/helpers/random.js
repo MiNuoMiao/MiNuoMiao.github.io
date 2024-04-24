8d5e6a1d0e15dcc9d48c53b11a892d3898cb868c
xiaomao <3172995085@qq.com>
2024-04-24 21:32:29 +0800

    更新主题1.6.6

--- themes/anzhiyu/scripts/helpers/random.js
+++ themes/anzhiyu/scripts/helpers/random.js
@@ -1,24 +1,24 @@
 hexo.extend.generator.register("random", function (locals) {
-  const config = hexo.config.random || {}
-  const themeConfig = hexo.theme.config
-  const randomNumberFriend = themeConfig.footer.list.randomFriends || 0
-  const posts = []
-  const link = locals.data.link || []
+  const config = hexo.config.random || {};
+  const themeConfig = hexo.theme.config;
+  const randomNumberFriend = themeConfig.footer.list.randomFriends || 0;
+  const posts = [];
+  const link = locals.data.link || [];
   for (const post of locals.posts.data) {
-    if (post.random !== false) posts.push(post.path)
+    if (post.random !== false) posts.push(post.path);
   }
 
-  const link_list = []
+  const link_list = [];
 
   link.forEach(element => {
     element.link_list.forEach(link_list_item => {
-      link_list.push(link_list_item)
-    })
-  })
+      link_list.push(link_list_item);
+    });
+  });
 
   let result = `var posts=${JSON.stringify(
     posts
-  )};function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};`
+  )};function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};`;
 
   if (themeConfig.footer.list.enable && randomNumberFriend > 0) {
     result += `var friend_link_list=${JSON.stringify(link_list)};
@@ -74,10 +74,10 @@ hexo.extend.generator.register("random", function (locals) {
       setTimeout(()=>{
         footerRandomFriendsBtn.style.opacity = "1";
       }, 300)
-    };`
+    };`;
   }
   return {
     path: config.path || "anzhiyu/random.js",
     data: result,
-  }
-})
+  };
+});
